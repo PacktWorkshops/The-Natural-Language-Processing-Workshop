b512cd97f19bd765696ec764703e4c2c8e322d00
Muzaffar <muzaffarshah@datoin.com>
2020-03-18 23:13:24 +0530

    re structured .py files in chapter 6 for unit tests

--- Chapter06/Exercise6.04/training_word_vctors.py
+++ Chapter06/Exercise6.04/training_word_vctors.py
@@ -1,32 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+import unittest
 
 import requests
 import json
 import re
 
-
-# In[ ]:
-
-
 with open('../data/ProjectGutenbergBooks.json', 'r') as catalog_file:
-        catalog = json.load(catalog_file)
+    catalog = json.load(catalog_file)
+
+print(catalog)
+
+GUTENBERG_URL = 'https://www.gutenberg.org/files/{}/{}-0.txt'
 
 
-# In[ ]:
-
-
-catalog
-
-
-# In[ ]:
-
-
-GUTENBERG_URL ='https://www.gutenberg.org/files/{}/{}-0.txt'
- 
 def load_book(book_id):
     url = GUTENBERG_URL.format(book_id, book_id)
     contents = requests.get(url).text
@@ -34,114 +19,75 @@ def load_book(book_id):
     return cleaned_contents
 
 
-# In[ ]:
-
-
-book_ids = [ book['id'] for book in catalog ]
-books = [ load_book(id) for id in book_ids]
-
-
-# In[ ]:
-
-
-books[:5]
-
-
-# In[ ]:
-
+book_ids = [book['id'] for book in catalog]
+books = [load_book(id) for id in book_ids]
+print(books[:5])
 
 from gensim.summarization import textcleaner
 from gensim.utils import simple_preprocess
- 
+
+
 def to_sentences(book):
-    """
-    >>> to_sentences("This is an example sentence. This is another example sentence.")
-    [['this', 'is', 'an', 'example', 'sentence'], ['this', 'is', 'another', 'example', 'sentence']]
-    """
     sentences = textcleaner.split_sentences(book)
     sentence_tokens = [simple_preprocess(sentence) for sentence in sentences]
     return sentence_tokens
 
 
-# In[ ]:
-
-
 books_sentences = [to_sentences(book) for book in books]
 documents = [sentence for book_sent in books_sentences for sentence in book_sent]
-
-
-# In[ ]:
-
-
-len(documents)
-
-
-# In[ ]:
-
+print(len(documents))
 
 from gensim.models import Word2Vec
+
 # build vocabulary and train model
 model = Word2Vec(
-        documents,
-        size=100,
-        window=10,
-        min_count=2,
-        workers=10)
+    documents,
+    size=100,
+    window=10,
+    min_count=2,
+    workers=10)
 model.train(documents, total_examples=len(documents), epochs=50)
 
-
-# In[ ]:
-
-
-model.wv.most_similar(positive="worse")
+print(model.wv.most_similar(positive="worse"))
 
 
-# ### The below method is just for test case
-
-# In[ ]:
-
-
+# The below method is just for test case
 def get_similar(word):
-    """
-    >>> get_similar('worse')
-    'better'
-    """
-    
     return model.wv.most_similar(positive=word)[1][0]
 
 
-# In[ ]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
- 
+
+
 def show_vector(word):
     vector = model.wv[word]
-    fig, ax = plt.subplots(1,1, figsize=(10, 2))
-    ax.tick_params(axis='both', 
+    fig, ax = plt.subplots(1, 1, figsize=(10, 2))
+    ax.tick_params(axis='both',
                    which='both',
-                 left=False, 
-                   bottom=False, 
+                   left=False,
+                   bottom=False,
                    top=False,
-                   labelleft=False, 
+                   labelleft=False,
                    labelbottom=False)
     ax.grid(False)
     print(word)
     ax.bar(range(len(vector)), vector, 0.5)
- 
+
+
 show_vector('sad')
 
 
-# In[ ]:
+class TestMethods(unittest.TestCase):
+
+    def test_get_similar(self):
+        result = 'narrower'
+        self.assertEqual(get_similar('worse'), result)
+
+    def test_to_sentences(self):
+        result = [['this', 'is', 'an', 'example', 'sentence'], ['this', 'is', 'another', 'example', 'sentence']]
+
+        self.assertEqual(to_sentences("This is an example sentence. This is another example sentence."), result)
 
 
-import doctest
-doctest.testmod(verbose=True)
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
--- Chapter06/Exercise6.05/using_pre_trained_vectors.py
+++ Chapter06/Exercise6.05/using_pre_trained_vectors.py
@@ -1,32 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+import unittest
 
 import numpy as np
 import zipfile
 
-
-# In[ ]:
-
-
-GLOVE_DIR = '../data/glove/'
-GLOVE_ZIP = GLOVE_DIR + 'glove6b50dtxt.zip'
+GLOVE_DIR = '../data/'
+GLOVE_ZIP = GLOVE_DIR + 'glove.6B.50d.txt.zip'
 print(GLOVE_ZIP)
- 
 
 zip_ref = zipfile.ZipFile(GLOVE_ZIP, 'r')
 zip_ref.extractall(GLOVE_DIR)
 zip_ref.close()
 
 
-# In[ ]:
-
-
 def load_glove_vectors(fn):
     print("Loading Glove Model")
-    with open( fn,'r', encoding='utf8') as glove_vector_file:
+    with open(fn, 'r', encoding='utf8') as glove_vector_file:
         model = {}
         for line in glove_vector_file:
             parts = line.split()
@@ -35,112 +23,61 @@ def load_glove_vectors(fn):
             model[word] = embedding
         print("Loaded {} words".format(len(model)))
     return model
- 
-glove_vectors = load_glove_vectors(GLOVE_DIR +'glove.6B.50d.txt')
 
 
-# In[ ]:
+glove_vectors = load_glove_vectors(GLOVE_DIR + 'glove.6B.50d.txt')
+
+print(glove_vectors)
+
+print(glove_vectors["dog"])
+
+print(glove_vectors["cat"])
 
 
-glove_vectors
-
-
-# In[ ]:
-
-
-glove_vectors["dog"]
-
-
-# In[ ]:
-
-
-glove_vectors["cat"]
-
-
-# ### the below method is just for test case and is not in exercise
-
-# In[ ]:
-
+# the below method is just for test case and is not in exercise
 
 def get_vector(word):
-    """
-    >>> get_vector("focus").mean()
-    0.003364140000000012
-    """
     return glove_vectors[word]
 
 
-# In[ ]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
- 
+
+
 def to_vector(glove_vectors, word):
     vector = glove_vectors.get(word.lower())
     if vector is None:
         vector = [0] * 50
-    return vector 
- 
+    return vector
+
+
 def to_image(vector, word=''):
-    fig, ax = plt.subplots(1,1)
+    fig, ax = plt.subplots(1, 1)
     ax.tick_params(axis='both', which='both',
-                   left=False, 
-                   bottom=False, 
+                   left=False,
+                   bottom=False,
                    top=False,
                    labelleft=False,
                    labelbottom=False)
     ax.grid(False)
     ax.bar(range(len(vector)), vector, 0.5)
-    ax.text(s=word, x=1, y=vector.max()+0.5)
+    ax.text(s=word, x=1, y=vector.max() + 0.5)
     return vector
 
 
-# In[ ]:
-
-
 man = to_image(to_vector(glove_vectors, "man"))
-
-
-# In[ ]:
-
-
 woman = to_image(to_vector(glove_vectors, "woman"))
-
-
-# In[ ]:
-
-
 king = to_image(to_vector(glove_vectors, "king"))
-
-
-# In[ ]:
-
-
 queen = to_image(to_vector(glove_vectors, "queen"))
-
-
-# In[ ]:
-
-
 diff = to_image(king - man + woman - queen)
-
-
-# In[ ]:
-
-
 nd = to_image(king - man + woman)
 
 
-# In[ ]:
+class TestMethods(unittest.TestCase):
+
+    def test_get_lemmas(self):
+        result = 0.003364140000000012
+        self.assertEqual(get_vector("focus").mean(), result)
 
 
-import doctest
-doctest.testmod(verbose=True)
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
