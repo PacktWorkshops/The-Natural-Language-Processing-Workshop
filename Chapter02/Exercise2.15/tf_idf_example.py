6a0067d6836679f0c475312d31ef0f8d83a31dc8
Muzaffar <muzaffarshah@datoin.com>
2020-03-19 10:14:50 +0530

    added more unit tests

--- Chapter02/Exercise2.15/tf_idf_example.py
+++ Chapter02/Exercise2.15/tf_idf_example.py
@@ -1,52 +1,33 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+import unittest
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# In[ ]:
-
-
 def get_tf_idf_vectors(corpus):
-    """
-    >>> float(get_tf_idf_vectors(["This is a sample text for testing", "This is again a sample text for testing"]).mean(axis=1).mean(axis=0)[0][0])
-    0.3622687874257512
-    """
+
     tfidf_model = TfidfVectorizer()
     vector_list = tfidf_model.fit_transform(corpus).todense()
     return vector_list
 
 
-# In[ ]:
-
-
 corpus = [
-        'Data Science is an overlap between Arts and Science',
-        'Generally, Arts graduates are right-brained and Science graduates are left-brained',
-        'Excelling in both Arts and Science at a time becomes difficult',
-        'Natural Language Processing is a part of Data Science'
-    ]
-
-
-# In[ ]:
-
+    'Data Science is an overlap between Arts and Science',
+    'Generally, Arts graduates are right-brained and Science graduates are left-brained',
+    'Excelling in both Arts and Science at a time becomes difficult',
+    'Natural Language Processing is a part of Data Science'
+]
 
 vector_list = get_tf_idf_vectors(corpus)
 print(vector_list)
 
 
-# In[ ]:
+class TestMethods(unittest.TestCase):
+
+    def test_get_tf_idf_vectors(self):
+        result = 0.3622687874257512
+        self.assertEqual(float(float(get_tf_idf_vectors(["This is a sample text for testing", "This is again a sample text for testing"]).mean(axis=1).mean(axis=0)[0][0])),
+            result)
 
 
-import doctest
-doctest.testmod(verbose=True)
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
--- Chapter02/Exercise2.17/leskalgorithm.py
+++ Chapter02/Exercise2.17/leskalgorithm.py
@@ -1,33 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Implementing lesk algorithm from scratch using string similarity and text vectorization
+import unittest
 
-# In[ ]:
-
-
-import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
-from nltk import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.datasets import fetch_20newsgroups
 import numpy as np
 
 
-# In[ ]:
-
-
 def get_tf_idf_vectors(corpus):
-    """
-    >>> float(get_tf_idf_vectors(["This is a test String", "This is an another test String"]).mean(axis=0).mean(axis=1))
-    0.36795725772534665
-    """
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_results = tfidf_vectorizer.fit_transform(corpus).todense()
     return tfidf_results
-
-
-# In[ ]:
 
 
 def to_lower_case(corpus):
@@ -35,63 +17,54 @@ def to_lower_case(corpus):
     return lowercase_corpus
 
 
-# In[ ]:
-
-
-def find_sentence_defnition(sent_vector,defnition_vectors):
+def find_sentence_defnition(sent_vector, defnition_vectors):
     """
     
     This method will find cosine similarity of sentence with
-    the possible definitionsdefnitions and return the one with highest similarity score
+    the possible definitions and return the one with highest similarity score
     along with the similarity score.
-    
-    >>> find_sentence_defnition(np.array([1,1,1, 0]).reshape(1,-1),\
-                       {'def1':np.array([1,1, 0, 0]).reshape(1,-1), 'def2':np.array([1,1, 1, 1]).reshape(1,-1)})
-    ('def2', 0.8660254037844388)
     
     """
     result_dict = {}
-    for defnition_id,def_vector in defnition_vectors.items():
-        sim = cosine_similarity(sent_vector,def_vector)
+    for defnition_id, def_vector in defnition_vectors.items():
+        sim = cosine_similarity(sent_vector, def_vector)
         result_dict[defnition_id] = sim[0][0]
-    defnition  = sorted(result_dict.items(), key=lambda x: x[1], reverse=True)[0]
-    return defnition[0],defnition[1]
-
-
-# In[ ]:
+    defnition = sorted(result_dict.items(), key=lambda x: x[1], reverse=True)[0]
+    return defnition[0], defnition[1]
 
 
 corpus = ["On the banks of river Ganga, there lies the scent of spirituality",
           "An institute where people can store extra cash or money.",
           "The land alongside or sloping down to a river or lake"
-           "What you do defines you",
-           "Your deeds define you",
-           "Once upon a time there lived a king.",
-           "Who is your queen?",
-            "He is desperate",
-           "Is he not desperate?"]
+          "What you do defines you",
+          "Your deeds define you",
+          "Once upon a time there lived a king.",
+          "Who is your queen?",
+          "He is desperate",
+          "Is he not desperate?"]
 
-
-# In[ ]:
-
-
-lower_case_corpus  = to_lower_case(corpus)
-corpus_tf_idf  = get_tf_idf_vectors(lower_case_corpus)
+lower_case_corpus = to_lower_case(corpus)
+corpus_tf_idf = get_tf_idf_vectors(lower_case_corpus)
 sent_vector = corpus_tf_idf[0]
-defnition_vectors = {'def1':corpus_tf_idf[1],'def2':corpus_tf_idf[2]}
-defnition_id, score  = find_sentence_defnition(sent_vector,defnition_vectors)
-print("The defnition of word {} is {} with similarity of {}".format('bank',defnition_id,score))
+defnition_vectors = {'def1': corpus_tf_idf[1], 'def2': corpus_tf_idf[2]}
+defnition_id, score = find_sentence_defnition(sent_vector, defnition_vectors)
+print("The defnition of word {} is {} with similarity of {}".format('bank', defnition_id, score))
 
 
-# In[ ]:
+class TestMethods(unittest.TestCase):
+
+    def test_find_sentence_defnition(self):
+        result = ('def2', 0.8660254037844388)
+        self.assertEqual(find_sentence_defnition(np.array([1, 1, 1, 0]).reshape(1, -1),
+                                                 {'def1': np.array([1, 1, 0, 0]).reshape(1, -1),
+                                                  'def2': np.array([1, 1, 1, 1]).reshape(1, -1)}), result)
+
+    def test_get_tf_idf_vectors(self):
+        result = 0.36795725772534665
+        self.assertEqual(float(
+            get_tf_idf_vectors(["This is a test String", "This is an another test String"]).mean(axis=0).mean(axis=1)),
+                         result)
 
 
-import doctest
-doctest.testmod(verbose=True)
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    unittest.main()
