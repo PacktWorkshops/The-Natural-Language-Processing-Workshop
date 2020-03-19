f4e4670172a648dd245000ee56eb4b7e1372b688
Muzaffar <muzaffarshah@datoin.com>
2020-03-19 10:14:50 +0530

    added more unit tests

--- Chapter02/Exercise2.16/text_similarity.py
+++ Chapter02/Exercise2.16/text_similarity.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+import unittest
 
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
 lemmatizer = WordNetLemmatizer()
 import numpy as np
-
-
-# In[ ]:
 
 
 def extract_text_similarity_jaccard(text1, text2):
@@ -21,7 +15,7 @@ def extract_text_similarity_jaccard(text1, text2):
     0.67
     """
     lemmatizer = WordNetLemmatizer()
- 
+
     words_text1 = [lemmatizer.lemmatize(word.lower()) for word in word_tokenize(text1)]
     words_text2 = [lemmatizer.lemmatize(word.lower()) for word in word_tokenize(text2)]
     nr = len(set(words_text1).intersection(set(words_text2)))
@@ -30,38 +24,20 @@ def extract_text_similarity_jaccard(text1, text2):
     return jaccard_sim
 
 
-# In[ ]:
-
-
 pair1 = ["What you do defines you", "Your deeds define you"]
 pair2 = ["Once upon a time there lived a king.", "Who is your queen?"]
 pair3 = ["He is desperate", "Is he not desperate?"]
 
+extract_text_similarity_jaccard(pair1[0], pair1[1])
 
-# In[ ]:
+extract_text_similarity_jaccard(pair2[0], pair2[1])
 
-
-extract_text_similarity_jaccard(pair1[0],pair1[1])
-
-
-# In[ ]:
-
-
-extract_text_similarity_jaccard(pair2[0],pair2[1])
-
-
-# In[ ]:
-
-
-extract_text_similarity_jaccard(pair3[0],pair3[1])
-
-
-# In[ ]:
+extract_text_similarity_jaccard(pair3[0], pair3[1])
 
 
 def get_tf_idf_vectors(corpus):
     """
-    >>> float(get_tf_idf_vectors(["This is a test string", "This is another test string"]).mean(axis=0).mean(axis=1))
+    >>>
     0.4211322281532753
     """
     tfidf_vectorizer = TfidfVectorizer()
@@ -69,47 +45,32 @@ def get_tf_idf_vectors(corpus):
     return tfidf_results
 
 
-# In[ ]:
-
-
 corpus = [pair1[0], pair1[1], pair2[0], pair2[1], pair3[0], pair3[1]]
 tf_idf_vectors = get_tf_idf_vectors(corpus)
 
+print(cosine_similarity(tf_idf_vectors[0], tf_idf_vectors[1]))
 
-# In[ ]:
+print(cosine_similarity(tf_idf_vectors[2], tf_idf_vectors[3]))
 
-
-cosine_similarity(tf_idf_vectors[0],tf_idf_vectors[1])
-
-
-# In[ ]:
+print(cosine_similarity(tf_idf_vectors[4], tf_idf_vectors[5]))
 
 
-cosine_similarity(tf_idf_vectors[2],tf_idf_vectors[3])
+class TestMethods(unittest.TestCase):
+
+    def test_cosine_similarity(self):
+        result = 0.5773502691896258
+
+        self.assertEqual(
+            cosine_similarity(np.array([1, 1, 1]).reshape(1, -1), np.array([0, 0, 1]).reshape(1, -1))[0][0],
+            result)
 
 
-# In[ ]:
+def test_get_tf_idf_vectors(self):
+    result = 0.36795725772534665
+    self.assertEqual(float(
+        get_tf_idf_vectors(["This is a test String", "This is an another test String"]).mean(axis=0).mean(axis=1)),
+        result)
 
 
-cosine_similarity(tf_idf_vectors[4],tf_idf_vectors[5])
-
-
-# In[ ]:
-
-
-def test_cosine_similarity(arr1,arr2):
-    """
-    >>> test_cosine_similarity(np.array([1,1,1]).reshape(1, -1), np.array([0,0,1]).reshape(1, -1))[0][0]
-    0.5773502691896258
-    
-    """
-    
-    return cosine_similarity(arr1, arr2)
-
-
-# In[ ]:
-
-
-import doctest
-doctest.testmod(verbose=True)
-
+if __name__ == '__main__':
+    unittest.main()
